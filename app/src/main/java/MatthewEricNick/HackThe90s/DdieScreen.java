0d8da1cbe7176b0b06fedad24a8e5d0384f15021
Eric <eric_lac@hotmail.com>
2019-02-09 15:06:36 -0800

    added death screen activity

---
+++ app/src/main/java/MatthewEricNick/HackThe90s/DdieScreen.java
@@ -0,0 +1,13 @@
+package MatthewEricNick.HackThe90s;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DdieScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ddie_screen);
+    }
+}
