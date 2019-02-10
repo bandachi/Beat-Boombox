70ad55aa7201b96638599638e75f9bb48c007927
Matthew Teja <matthew.teja@gmail.com>
2019-02-09 16:01:18 -0800

    Added die screen

--- app/src/main/java/MatthewEricNick/HackThe90s/MainActivity.java
+++ app/src/main/java/MatthewEricNick/HackThe90s/MainActivity.java
@@ -3,6 +3,7 @@ package MatthewEricNick.HackThe90s;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,7 +15,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void changeToGame(View view) {
+        findViewById(R.id.playButton).setAlpha(0.5f);
         Intent intent = new Intent(this, GameActivity.class);
         startActivity(intent);
     }
+
 }
